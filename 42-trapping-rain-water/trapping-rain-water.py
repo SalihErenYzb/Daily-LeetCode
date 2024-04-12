8babb3d9b3135a3cb0c401fa9d6be2ef89a7d727
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-04-12 22:55:53 +0300

    Time: 115 ms (23.68%) | Memory: 18.7 MB (16.57%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,28 +1,20 @@
 class Solution:
-    def trap(self, height: List[int]) -> int:
+    def trap(self, height) -> int:
         ans = 0
-        max1 = height[0]
-        temp = 0
-        for hei in height[1:]:
-
-            if hei >= max1:
-                max1 = hei
-                ans += temp
-                temp = 0
-            print(hei,abs(hei-max1))
-            temp += abs(hei-max1)
-        height= list(reversed(height))
-        max1 = height[0]
-        temp = 0
-        for hei in height[1:]:
-
-            if hei > max1:
-                max1 = hei
-                ans += temp
-                temp = 0
-            print(hei,abs(hei-max1))
-
-            temp += abs(hei-max1)
+        max1 = [height[0],height[-1]]
+        temp = [0,0]
+        for i in range(1,len(height)):
+            hei = [height[i],height[-i-1]]
+            if hei[0] >= max1[0]:
+                max1[0] = hei[0]
+                ans += temp[0]
+                temp[0] = 0
+            if hei[1] > max1[1]:
+                max1[1] = hei[1]
+                ans += temp[1]
+                temp[1] = 0
+            temp[0] += abs(hei[0]-max1[0])
+            temp[1] += abs(hei[1]-max1[1])
         return ans
         
 
