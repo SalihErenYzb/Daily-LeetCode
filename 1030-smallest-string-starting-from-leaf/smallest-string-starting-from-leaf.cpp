7c7f329fcb86be05d9769f8fe152a29b14701553
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-04-17 16:43:18 +0300

    Time: 6 ms (82.98%) | Memory: 19.8 MB (84.69%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     string smallestFromLeaf(TreeNode* root) {
-        string ans = "zzzzzzzzzzz";
-        string tmp = "";
+        deque<char> ans(10,'z');
+        deque<char> tmp;
         dfs(root,tmp,ans);
-        return ans;
+        return string(ans.begin(),ans.end());
     }
-    void dfs(TreeNode* root, string& st,string& ans){
-        st = char('a' + root->val) + st;
+    void dfs(TreeNode* root, deque<char>& st,deque<char>& ans){
+        st.emplace_front(char('a' + root->val));
         if (root->left == nullptr && root->right == nullptr){
-            ans = min(ans,st);
-            st.erase(0,1);
+            bool b = lexicographical_compare(st.begin(),st.end(),ans.begin(),ans.end()); 
+            if (b)
+                ans = st;
+            st.pop_front();
             return;
         }
         if (root->left != nullptr)
             dfs(root->left,st,ans);
         if (root->right != nullptr)
             dfs(root->right, st,ans);
-        st.erase(0,1);
+        st.pop_front();
     }
 };
