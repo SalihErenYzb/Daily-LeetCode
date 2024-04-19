28caea7b025dea73aaf91e13db678b6ffa63e745
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-04-19 10:31:52 +0300

    Time: 42 ms (15.83%) | Memory: 20.5 MB (35.18%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -5,26 +5,25 @@ public:
             return true;
         return false;
     }
-    void dfs(vector<vector<bool>>& vis, vector<vector<char>>& grid, int i, int j){
-        vis[i][j] = true;
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        grid[i][j] = '2';
         vector<tuple<int,int>> tmp = {{1,0}, {-1,0}, {0,1}, {0,-1}};
         for (auto [_i,_j]: tmp){
             int newI = i+_i;
             int newJ = j+_j;
-            if (isG(grid,newI,newJ) && !vis[newI][newJ]){
-                dfs(vis,grid,newI,newJ);
+            if (isG(grid,newI,newJ)){
+                dfs(grid,newI,newJ);
             }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
         
         int ans = 0;
-        vector<vector<bool>> vis(grid.size(),vector<bool>(grid[0].size(),0));
         for (int i = 0; i < grid.size(); i++)
             for (int j = 0; j < grid[0].size(); j++){
                 //cout << i << " " << j << " " << vis[i][j] << "\n";
-                if(!vis[i][j] && grid[i][j] == '1'){
-                    dfs(vis,grid,i,j);
+                if(grid[i][j] == '1'){
+                    dfs(grid,i,j);
                     ans++;
                 }
                     
