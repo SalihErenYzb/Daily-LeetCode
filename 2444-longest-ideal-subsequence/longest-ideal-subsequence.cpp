e6debaad11e8ca0e16ea0e0f3cc067dfac286471
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-04-25 12:28:56 +0300

    Time: 40 ms (84.03%) | Memory: 11.5 MB (84.45%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -3,18 +3,15 @@ public:
     int longestIdealString(string s, int k) {
         int n = s.size();
         vector<int> dp(26,0);
+        int maxAll = 0;
         for (int i = 1; i <= n; i++){
             int c = s.at(i-1) - 'a';
-            dp[c]++;
-            for (int j = max(0,c-k); j <= min(25,c+k); j++){
-                if (j != c)
-                    dp[c] = max(dp[c],dp[j] + 1);
-            }
+            int maxi = 0;
+            for (int j = max(0,c-k); j <= min(25,c+k); j++)
+                maxi = max(maxi,dp[j]);
+            dp[c] = ++maxi;
+            maxAll = max(maxAll,maxi); 
         }
-        int maxi = 0;
-        for (int i = 0; i < 26; i++)
-            maxi = max(dp[i],maxi);
-        return maxi;
-        
+        return maxAll;
     }
 };
