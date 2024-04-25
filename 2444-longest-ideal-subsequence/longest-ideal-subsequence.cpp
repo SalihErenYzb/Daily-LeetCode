ed0af192a64f8a2d6fb1c4479e68ca4bfad3ff0f
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-04-25 12:12:45 +0300

    Time: 50 ms (70.17%) | Memory: 11.5 MB (84.45%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     int longestIdealString(string s, int k) {
         int n = s.size();
-        vector<vector<int>> dp(n+1,vector<int>(27,0));
+        vector<int> dp(27,0);
         for (int i = 1; i <= n; i++){
             int c = s.at(i-1) - 'a' + 1;
             //7cout << s.at(i-1) << " " << c << "\n";
+            int maxi = 0;
             for (int j = 1; j < 27; j++){
                 int d = abs(c-j);
-                dp[i][j] = max(dp[i-1][j],dp[i][j]);
                 if (k >= d)
-                    dp[i][c] = max(dp[i][c],dp[i-1][j] + 1);
+                    maxi = max(maxi,dp[j] + 1);
             }
+            dp[c] = maxi;
             //for (int j =1 ; j < 27; j++){
             //    cout << dp[i][j] << "\n";
             //}
         }
         int maxi = 0;
         for (int i = 1; i < 27; i++)
-            maxi = max(dp[n][i],maxi);
+            maxi = max(dp[i],maxi);
         return maxi;
         
     }
