444cc287c2aca8e5bb2f995ef8ed508f1f28998b
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-04-26 22:38:07 +0300

    Time: 20 ms (97.28%) | Memory: 18.1 MB (57.39%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -6,17 +6,17 @@ public:
         int n = grid.size();
         vector<int> dp(n,0);
         for (auto row: grid){
+            int ssm = 999999;
+            int sm = 999999;
             for (int i = 0; i < n; i++){
                 dp[i] = dp[i] != smallestEl ? row[i] + smallestEl : row[i] + ssmallest;
+                if (ssm > dp[i])
+                    ssm = dp[i];
+                if (sm > dp[i])
+                    swap(sm,ssm);
             }
-            ssmallest = 999999;
-            smallestEl = 999999;
-            for (int i = 0; i < n; i++){
-                if (ssmallest > dp[i])
-                    ssmallest = dp[i];
-                if (smallestEl > dp[i])
-                    swap(smallestEl,ssmallest);
-            }
+            smallestEl = sm;
+            ssmallest = ssm;
         }
         return smallestEl;
     }
