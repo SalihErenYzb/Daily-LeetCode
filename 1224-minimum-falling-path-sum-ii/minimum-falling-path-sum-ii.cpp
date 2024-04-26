51339559695dacc3c3191c3c1b8edb200ec2a07a
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-04-26 22:50:54 +0300

    Time: 22 ms (93.19%) | Memory: 17 MB (86.58%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
-        int smallest = 0;
-        int ssmallest = 0;
+        int smallest = 0,ssmallest = 0;
         int n = grid.size();
         for (auto& row: grid){
-            int ssm = 999999;
-            int sm = 999999;
+            int ssm = 1e9,sm = 1e9;
             for (int i = 0; i < n; i++){
                 grid[0][i] = grid[0][i] != smallest ? row[i] + smallest : row[i] + ssmallest;
                 if (ssm > grid[0][i])
@@ -14,8 +12,7 @@ public:
                 if (sm > grid[0][i])
                     swap(sm,ssm);
             }
-            smallest = sm;
-            ssmallest = ssm;
+            smallest = sm,ssmallest = ssm;
         }
         return smallest;
     }
