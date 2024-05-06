000cab7d6f74012ae406db72d866cfbcc95d476d
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-05-06 16:33:52 +0300

    Time: 363 ms (5.00%) | Memory: 170.9 MB (27.65%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        int max1 = -1;
+        recursiveMax(head,max1);
+        return head;
+    }
+    void recursiveMax(ListNode*& node, int& max1){
+        if (node->next != nullptr){
+            recursiveMax(node->next,max1);
+        }
+        if (node->val < max1){
+            ListNode* tmp = node;
+            node = node->next;
+            delete tmp;
+        }
+        max1 = max(max1,node->val);
+    }
+};
