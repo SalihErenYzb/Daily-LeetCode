09596871650a44cdd6905439d867662a5ac7ba2e
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-06-11 18:09:24 +0300

    Time: 0 ms (100.00%) | Memory: 10.8 MB (6.31%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -5,12 +5,11 @@ public:
         int n = arr2.size();
         for (int i = 0; i < arr2.size(); i++)
             value_map[arr2[i]] = i;
-        auto comp = [&value_map,n](int a, int b) {
+        sort(begin(arr1),end(arr1),[&value_map,n](int a, int b) {
             int rankA = value_map.count(a) ? value_map[a] : 1e5*n + a;
             int rankB = value_map.count(b) ? value_map[b] : 1e5*n + b;
             return rankA < rankB;
-        };
-        sort(begin(arr1),end(arr1),comp);
+        });
         return arr1;
     }
 };
