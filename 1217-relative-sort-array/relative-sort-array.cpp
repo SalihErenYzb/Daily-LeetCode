0ecc67a092d26dabe2042e60d052b9794c4baf44
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-06-11 18:14:25 +0300

    Time: 6 ms (17.85%) | Memory: 10.7 MB (6.31%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -3,12 +3,10 @@ public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         unordered_map<int,int> value_map;
         int n = arr2.size();
-        for (int i = 0; i < arr2.size(); i++)
-            value_map[arr2[i]] = i;
+        for (int i = 0; i < arr2.size(); i++) value_map[arr2[i]] = i;
         sort(begin(arr1),end(arr1),[&value_map,n](int a, int b) {
-            int rankA = value_map.count(a) ? value_map[a] : 1e5*n + a;
-            int rankB = value_map.count(b) ? value_map[b] : 1e5*n + b;
-            return rankA < rankB;
+            return (value_map.count(a) ? value_map[a] : 1e5*n + a) <
+            (value_map.count(b) ? value_map[b] : 1e5*n + b);
         });
         return arr1;
     }
