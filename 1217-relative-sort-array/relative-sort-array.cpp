0ba5800e1ec74b7dc3c1c20bf1ab4f28aa260369
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-06-11 18:08:45 +0300

    Time: 0 ms (100.00%) | Memory: 10.7 MB (6.31%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -2,20 +2,13 @@ class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         unordered_map<int,int> value_map;
+        int n = arr2.size();
         for (int i = 0; i < arr2.size(); i++)
             value_map[arr2[i]] = i;
-        auto comp = [&value_map](int a, int b) {
-            auto foundA = value_map.find(a);
-            auto foundB = value_map.find(b);
-            if (foundA != value_map.end() && foundB != value_map.end()) {
-                return foundA->second < foundB->second;
-            } else if (foundA != value_map.end()) {
-                return true;
-            } else if (foundB != value_map.end()) {
-                return false;
-            } else {
-                return a < b;
-            }
+        auto comp = [&value_map,n](int a, int b) {
+            int rankA = value_map.count(a) ? value_map[a] : 1e5*n + a;
+            int rankB = value_map.count(b) ? value_map[b] : 1e5*n + b;
+            return rankA < rankB;
         };
         sort(begin(arr1),end(arr1),comp);
         return arr1;
