a84fb506211aa234eb4057b88fc10c252453c131
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-06-17 00:57:05 +0300

    Time: 53 ms (85.13%) | Memory: 51.8 MB (33.54%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let num = init;
+    return {
+        increment: () => ++num,
+        decrement: () => --num,
+        reset: () => num = init,        
+    }  
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
