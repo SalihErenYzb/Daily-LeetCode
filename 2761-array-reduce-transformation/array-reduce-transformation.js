c136114c18dacebb8d2c22de7d8f46ab2c7710fd
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-06-21 00:12:22 +0300

    Time: 48 ms (86.56%) | Memory: 51.4 MB (6.15%) - LeetSync

---
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    for (let el of nums)
+        init = fn(init,el);
+    return init;
+};
