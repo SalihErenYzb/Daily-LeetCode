1a0ed0feae5a09dc22122ca1252338a1095a1c53
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-06-24 16:06:54 +0300

    Time: 86 ms (75.21%) | Memory: 108.6 MB (96.72%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
-        int i = 0;
-        int flag = false;
         int ans = 0;
-        while (i+k <= nums.size()){
-            nums[i+k-1] ^= flag;
-            if (!(nums[i++]^flag)){
-                flag = flag^1;
-                ans++;
-            }
+        for (int i = 0,flag=0; i < nums.size();i++) { 
+            (i + k <= nums.size()) && ((nums[i + k - 1] ^= flag) ^ (!(nums[i] ^ flag) && ++ans && (flag ^= 1)));
+            if (!(nums[i] ^ flag)) return -1;
         }
-        while (i < nums.size())
-            if (!(nums[i++]^flag)) return -1;
         return ans;
     }
 };
