4521a8bed6c46828e73761e70e855e059284079e
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-07-13 22:20:14 +0300

    Time: 567 ms (9.16%) | Memory: 204.4 MB (73.28%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        vector<pair<int,int>> righters;
+        vector<int> ans;
+        vector<pair<int,int>> tmpForSort;
+        for (int i = 0; i < positions.size(); i++)
+            tmpForSort.push_back({positions[i],i});
+        sort(begin(tmpForSort),end(tmpForSort));
+        for (int j = 0; j < positions.size(); j++){
+            int i = tmpForSort[j].second;
+            cout << positions[i] << " " << healths[i] << " " << directions[i] << "\n";
+            if (directions.at(i) == 'R'){
+                righters.push_back({i,healths[i]});
+            }else{
+                while (healths[i]){
+                    if (!righters.size()){
+                        ans.push_back(i);
+                        break;
+                    }
+                    auto& fighter = righters[righters.size()-1];
+                    if (healths[i] > fighter.second){
+                        righters.pop_back();
+                        healths[i]--;
+                    }else if (healths[i] == fighter.second){
+                        healths[i] = 0;
+                        righters.pop_back();
+                    }else{
+                        fighter.second--;
+                        healths[i] = 0;
+                    }
+                }
+            }
+        }
+        for (auto& num: righters){
+            ans.push_back(num.first);
+            healths[num.first] = num.second;
+        }
+        sort(begin(ans),end(ans));
+        for (int& a: ans)
+            a = healths[a];
+        return ans;
+    }
+};
