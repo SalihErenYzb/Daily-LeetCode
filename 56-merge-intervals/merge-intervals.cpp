8272388e6b9087dc83a228a648941083a09bd4c8
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-09-24 20:02:22 +0300

    Time: 25 ms (45.62%) | Memory: 23 MB (72.73%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    static bool compare(vector<int> a, vector<int> b){
-        if (a[0] != b[0]) return a[0] < b[0];
-        return a[1] < b[1];
-    }
+
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(begin(intervals),end(intervals),compare);
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        });        
         vector<vector<int>> answer;
         for (int i = 0; i < intervals.size()-1; i++){
             vector<int>& first = intervals[i];
