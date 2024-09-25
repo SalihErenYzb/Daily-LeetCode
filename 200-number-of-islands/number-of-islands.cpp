dbd537437e3fbc315decdbd7701929f8a803c952
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-09-25 11:49:54 +0300

    Time: 26 ms (74.55%) | Memory: 18 MB (39.05%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
-    bool isG(vector<vector<char>>& grid, int i, int j){
-        if (i >= 0 && i < grid.size() && j >= 0 && j < grid[0].size() && grid[i][j] == '1')
-            return true;
-        return false;
-    }
-    void dfs(vector<vector<char>>& grid, int i, int j){
-        grid[i][j] = '2';
-        vector<tuple<int,int>> tmp = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-        for (auto [_i,_j]: tmp){
-            int newI = i+_i;
-            int newJ = j+_j;
-            if (isG(grid,newI,newJ)){
-                dfs(grid,newI,newJ);
+    int answer = 0;
+    void dfs(vector<vector<char>>& grid,int i, int j){
+        vector<pair<int,int>> st;
+        st.push_back({i,j});
+        while (!st.empty()){
+            auto curr = st.back();st.pop_back();
+            int k = curr.first;
+            int m = curr.second;
+            if (grid[k][m] == '0') continue;
+            grid[k][m] = 0;
+            if (k > 0 && grid[k-1][m] == '1'){
+                st.push_back({k-1,m});
+            }
+            if (m > 0 && grid[k][m-1] == '1'){
+                st.push_back({k,m-1});
+            }
+            if (m < grid[0].size()-1 && grid[k][m+1] == '1'){
+                st.push_back({k,m+1});
+            }
+            if (k < grid.size()-1 && grid[k+1][m] == '1'){
+                st.push_back({k+1,m});
             }
         }
+        answer++;
     }
     int numIslands(vector<vector<char>>& grid) {
-        
-        int ans = 0;
-        for (int i = 0; i < grid.size(); i++)
+        for (int i = 0; i < grid.size(); i++){
             for (int j = 0; j < grid[0].size(); j++){
-                //cout << i << " " << j << " " << vis[i][j] << "\n";
-                if(grid[i][j] == '1'){
+                if (grid[i][j] == '1'){
                     dfs(grid,i,j);
-                    ans++;
                 }
-                    
             }
-        return ans;
+        }
+        return answer;
     }
 };
