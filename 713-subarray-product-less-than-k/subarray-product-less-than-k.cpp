ac184ad12015e00ac086d1aa0097490eabd40c2b
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-09-30 22:44:51 +0300

    Time: 13 ms (99.81%) | Memory: 15.2 MB (99.90%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -1,3 +1,77 @@
+#pragma GCC optimize("Ofast","inline","fast-math","unroll-loops","no-stack-protector","-ffast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+inline bool is_digit_or_negative(char c) {
+    return (c >= '0' && c <= '9') || (c == '-');
+}
+
+std::vector<int> parse_input_data_string(const std::string& s) {
+    std::vector<int> input_data_1D;
+    const int N = s.size();
+    int left = 0;
+    while (left < N) {
+        if (!is_digit_or_negative(s[left])) {
+            ++left;
+            continue;
+        }
+        int right = left;
+        long long value = 0;
+        int is_negative = 1;
+        while (right < N && is_digit_or_negative(s[right])) {
+            if (s[right] == '-') {
+                is_negative = -1;
+            } else {
+                value = value * 10 + (s[right] - '0');
+            }
+            ++right;
+        }
+        value *= is_negative;
+        input_data_1D.push_back(value);
+        left = right;
+    }
+    return input_data_1D;
+}
+
+int numSubarrayProductLessThanK(std::vector<int>& nums, int k) {
+    const int N = nums.size();
+    if (k <= 1) {
+        return 0;
+    }
+    int counter = 0;
+    int product = 1;
+    int l = 0;
+    int r = 0;
+    while (r < N) {
+        product *= nums[r];
+        while (l <= r && product >= k) {
+            product /= nums[l];
+            ++l;
+        }
+        counter += r - l + 1;
+        ++r;
+    }
+    return counter;
+}
+
+bool Solve = [](){
+    std::ofstream out("user.out");
+    for (std::string s; std::getline(std::cin, s);) {
+        std::vector<int> input_data = parse_input_data_string(s);
+        std::getline(std::cin, s);
+        const int k = std::stoi(s);
+        out << numSubarrayProductLessThanK(input_data, k) << "\n";
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
