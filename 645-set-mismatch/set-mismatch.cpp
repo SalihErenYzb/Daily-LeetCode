34cd7d508f9d3360e033b7afa0faa780ca33e94a
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-09-30 13:32:20 +0300

    Time: 28 ms (45.82%) | Memory: 24 MB (93.14%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        nums.push_back(nums.size()+10);
-        int ans = 0;
-        int sum = 0;
-        for (int i = 0; i < nums.size()-1; i++){
-            sum += abs(nums[i]);
-            nums[abs(nums[i])] *= -1;
-            if (nums[abs(nums[i])] > 0){
-                ans = abs(nums[i]);
+        int n=nums.size();
+        // sum = (n*(n+1))/2
+        // actualSum= sum-missing+dupli - (1)
+        // sumSq=(n(n+1)2n+1))/6
+        // actualSumSq=sumSq - missingSq + dupliSq = k + (dupli-miss)(dupli+miss)
+        // use 1 in above eqn
+        // we get both a+b and a-b
+
+        // xor method
+        int xorr=0;
+        for(int i=1;i<=n;i++){
+            xorr^=(i^nums[i-1]);
+        }
+        // final result= a^b
+        // sepearte the array in two parts
+        int firstDiffBit=0;
+        for(int i=0;i<32;i++){
+            if((xorr&(1<<i))!=0){
+                firstDiffBit=i;
+                break;
             }
         }
-        int n = nums.size()-1;
-        sum -= (n*(n+1))/2;
-        for (int& num: nums)
-            num = abs(num);
-        return {ans,ans-sum};
+        int xorr1=0,xorr2=0;
+        for(int i=1;i<=n;i++){
+            if((nums[i-1]&(1<<firstDiffBit))!=0){
+                xorr1^=nums[i-1];
+            }
+            else{
+                xorr2^=nums[i-1];
+            }
+            if((i&(1<<firstDiffBit))!=0){
+                xorr1^=i;
+            }
+            else{
+                xorr2^=i;
+            }
+        }
+        for(auto num:nums){
+            if(num==xorr1)return {xorr1,xorr2};
+        }
+        return {xorr2,xorr1};
     }
 };
