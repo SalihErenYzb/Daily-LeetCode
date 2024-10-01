16bc7c57ce13cc6133960c1cdbb29b70ac6a364b
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-10-01 20:51:05 +0300

    Time: 69 ms (96.56%) | Memory: 64.4 MB (76.85%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -8,17 +8,12 @@ public:
             }
             kc[(num%k + k)%k]++;
         }
-        // k = 5
-        // 0 1 2 3 4
-        // k = 10
-        // 0 1 2 3 4 5 6 7 8 9
-        // 0 0 1 1 0 0 0 1 1 0
         for (int i = 1; i <= k/2; i++){
             if (kc[i] != kc[k-i]){
                 return false;
             }
         }
-        if (kc[0]%2 || (k%2 == 0 && kc[k/2]%2)){
+        if (kc[0]%2){
             return false;
         }
         return true;
