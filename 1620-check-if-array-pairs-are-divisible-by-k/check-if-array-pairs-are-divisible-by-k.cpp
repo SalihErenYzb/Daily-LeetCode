0d7edc443a96d1c397539576ee7e8d09bfaea08f
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-10-01 20:50:06 +0300

    Time: 143 ms (28.17%) | Memory: 64.4 MB (88.62%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        vector<int> kc(k,0);
+        for (int num: arr){
+            if (num < 0){
+
+            }
+            kc[(num%k + k)%k]++;
+        }
+        // k = 5
+        // 0 1 2 3 4
+        // k = 10
+        // 0 1 2 3 4 5 6 7 8 9
+        // 0 0 1 1 0 0 0 1 1 0
+        for (int n: kc){
+            cout << n << " ";
+        }
+        for (int i = 1; i <= k/2; i++){
+            if (kc[i] != kc[k-i]){
+                cout << "harra";
+                return false;
+            }
+        }
+        if (kc[0]%2 || (k%2 == 0 && kc[k/2]%2)){
+            return false;
+        }
+        return true;
+    }
+};
