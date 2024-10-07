fa12f9676cf0df417f61e2e87326a1b413f8ef0b
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-10-07 16:31:30 +0300

    Time: 7 ms (51.96%) | Memory: 10.5 MB (81.55%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int minLength(string s) {
-        string a = "";
-        for (int i = 0; i < s.size(); i++){
-            if (!a.empty() && a.back() == 'A' && s[i] == 'B'){
-                a.pop_back();
-            }else if (!a.empty() && a.back() == 'C' && s[i] == 'D'){
+        string a = "^";
+        for (char c: s){
+            if (a.back() == 'A' && c == 'B' || a.back() == 'C' && c == 'D'){
                 a.pop_back();
             }else{
-                a += s[i];
+                a += c;
             }
         }
-        return a.size();
+        return a.size()-1;
     }
 };
