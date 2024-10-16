646e9f48e4cdf8dad386246f396840a816296322
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-10-16 17:14:10 +0300

    Time: 92 ms (99.46%) | Memory: 112.2 MB (75.81%) - LeetSync

--- 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.cpp
+++ 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.cpp
@@ -3,8 +3,8 @@ public:
     int maximumRobots(vector<int>& a, vector<int>& b, long long c) {
         ios_base::sync_with_stdio(false); deque<int> d; int e = 0; long long f = 0; 
         for (int g = 0; g < a.size(); g++) {
-            e++; f += b[g]; while (!d.empty() && d.back() < a[g]) d.pop_back(); 
-            d.push_back(a[g]); if (f * e + d.front() > c && e > 0) {
+            f += b[g]; while (!d.empty() && d.back() < a[g]) d.pop_back(); 
+            d.push_back(a[g]); if (f * ++e + d.front() > c && e > 0) {
                 f -= b[g - --e]; if (a[g - e] == d.front()) d.pop_front();
             }
         }
