b9c965c6321b21ee1a6ae89ec6d689b8434fff35
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-10-16 15:14:54 +0300

    Time: 85 ms (99.46%) | Memory: 112.2 MB (75.81%) - LeetSync

--- 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.cpp
+++ 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    int maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
-        ios_base::sync_with_stdio(false);
-        deque<int> maxest;
-        int size = 0;
-        long long sum = 0;
-        for (int i = 0; i < chargeTimes.size(); i++){
-            size++;
-            sum += runningCosts[i];
-            while (!maxest.empty() && maxest.back() < chargeTimes[i]) maxest.pop_back();
-            maxest.push_back(chargeTimes[i]);
-            if (sum*size + maxest.front() > budget && size > 0){
-                sum -= runningCosts[i- --size];
-                if (chargeTimes[i-size] == maxest.front()) maxest.pop_front();
+    int maximumRobots(vector<int>& a, vector<int>& b, long long c) {
+        ios_base::sync_with_stdio(false); deque<int> d; int e = 0; long long f = 0; 
+        for (int g = 0; g < a.size(); g++) {
+            e++; f += b[g]; while (!d.empty() && d.back() < a[g]) d.pop_back(); 
+            d.push_back(a[g]); if (f * e + d.front() > c && e > 0) {
+                f -= b[g - --e]; if (a[g - e] == d.front()) d.pop_front();
             }
         }
-        return size;
+        return e;
     }
 };
