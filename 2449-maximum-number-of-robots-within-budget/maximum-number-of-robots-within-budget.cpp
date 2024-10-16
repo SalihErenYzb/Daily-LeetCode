0c36e0c9cfa216f93e45738f7d9728e33b731dad
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-10-16 15:09:31 +0300

    Time: 134 ms (84.95%) | Memory: 112.4 MB (70.16%) - LeetSync

--- 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.cpp
+++ 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
-        deque<int> maxest; // hold maxes
-        long long size = 0;
+        deque<int> maxest;
+        int size = 0;
         long long sum = 0;
         for (int i = 0; i < chargeTimes.size(); i++){
             size++;
             sum += runningCosts[i];
-            // update maxesst
             while (!maxest.empty() && maxest.back() < chargeTimes[i]){
                 maxest.pop_back();
             }
             maxest.push_back(chargeTimes[i]);
-
-            // delete size if necessery
             if (sum*size + maxest.front() > budget && size > 0){
-                size--;
-                sum -= runningCosts[i-size];
+                sum -= runningCosts[i- --size];
                 if (chargeTimes[i-size] == maxest.front()){
                     maxest.pop_front();
                 }
