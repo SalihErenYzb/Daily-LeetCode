2fd78c88a2821811579259aa9ce46f469248d52e
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-10-16 23:45:11 +0300

    Time: 93 ms (99.47%) | Memory: 112.3 MB (75.80%) - LeetSync

--- 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.cpp
+++ 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.cpp
@@ -6,25 +6,18 @@ public:
         deque<int> maxes;
         int l = 0;
         long long sum = 0;
-        int maxLen = 0;
         for(int r = 0; r < n; r++) {
             while(!maxes.empty() && maxes.back() < chargeTimes[r]) {
                 maxes.pop_back();
             }
             maxes.push_back(chargeTimes[r]);
             sum += runningCosts[r];
-            while(l < r && maxes.front() + (r - l + 1) * sum > budget) {
+            if(maxes.front() + (r - l + 1) * sum > budget) {
                 if(chargeTimes[l] == maxes.front())
                     maxes.pop_front();
                 sum -= runningCosts[l++];
             }
-            //for (int i: maxes){
-            //    cout << i << " ";
-            //}
-            //cout << "\n";
-            if(maxes.front() + (r - l + 1) * sum <= budget)
-                maxLen = max(maxLen, r - l + 1);
         }
-        return maxLen;
+        return n-l;
     }
 };
