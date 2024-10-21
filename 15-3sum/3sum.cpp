eefa58a8934c88a505e26ae7a5cffa75e5dda287
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-10-21 20:53:18 +0300

    Time: 47 ms (97.28%) | Memory: 27.4 MB (71.56%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,34 +1,37 @@
+//Optimized Approach - O(n^2 logn + nlogn) - o(n^2 logn) time and O(n) space
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        unordered_map<int,int> m;
-        sort(begin(nums),end(nums));
-        for (auto num: nums){
-            m[num]++;
-        }
-        vector<vector<int>> ans;
+        int target = 0;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> output;
         int last = INT_MAX;
+        int last2;
         for (int i = 0; i < nums.size(); i++){
-            m[nums[i]]--;
-            if (nums[i] == last){
-                continue;
-            }
+            if (last == nums[i]) continue;
             last = nums[i];
-            int last2 = INT_MAX;
-            for (int j = i+1; j < nums.size(); j++){
-                m[nums[j]]--;
+            int j = i + 1;
+            int k = nums.size() - 1;
+            last2 = INT_MAX;
+            while (j < k) {
                 if (nums[j] == last2){
+                    j++;
                     continue;
                 }
                 last2 = nums[j];
-                if (m[-nums[i]-nums[j]] > 0){
-                    ans.push_back({nums[i],nums[j],-nums[i]-nums[j]});
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    output.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                    last2 = INT_MAX;
                 }
             }
-            for (int j = i+1; j < nums.size(); j++){
-                m[nums[j]]++;
-            }
         }
-        return ans;
+        return output;
     }
 };
