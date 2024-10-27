b5cca91f4a19740c79f30add6390600f12bb2d8d
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-10-27 22:31:06 +0300

    Time: 7 ms (51.85%) | Memory: 26.6 MB (78.35%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -4,12 +4,11 @@ public:
         vector<int> dp(matrix[0].size()+1,0);
         vector<int> dp2(matrix[0].size()+1,0);
         int ans = 0;
-        int continous;
+        int tmp;
         for (int i = 1; i <= matrix.size(); i++){
-            continous = 0;
             for (int j = 1; j <= matrix[0].size(); j++){
-                continous = matrix[i-1][j-1]*(continous+1);
-                dp2[j] = min(min(continous,dp[j-1]+1),dp[j]+1);
+                tmp = matrix[i-1][j-1]*(dp2[j-1]+1);
+                dp2[j] = min(min(tmp,dp[j-1]+1),dp[j]+1);
                 ans += dp2[j];
             }
             swap(dp,dp2);
