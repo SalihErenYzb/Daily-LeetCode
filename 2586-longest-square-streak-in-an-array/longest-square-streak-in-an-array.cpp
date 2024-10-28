5fd3ef02fe04d387e1035c2bae35b118d3770d53
SalihErenYzb <128384160+SalihErenYzb@users.noreply.github.com>
2024-10-28 11:28:47 +0300

    Time: 5 ms (100.00%) | Memory: 85.8 MB (99.36%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        int n = nums.size(), mx = 0;
+        int n = nums.size();
 
         int dp[100001];
         memset(dp, 0, sizeof(dp));
 
         for(int i = 0; i < n; i++){
             dp[nums[i]] = 1;
-            mx = max(mx, nums[i]);
         }
 
         int ans = -1;
-        for(int i = 2; i <= mx; i++){
-            if (i > 316) break;
+        for(int i = 2; i <= 316; i++){
             if(dp[i] && dp[i*i]){
                 dp[i*i] = dp[i]+1;
                 ans = max(ans, dp[i*i]);
